7694cb504c8101cbacafcdd8fd58f6229b43be5d
AlanDBalan <132862280+AlanDBalan@users.noreply.github.com>
2023-07-27 16:50:50 -0500

    Added click event listener to save text to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -6,12 +6,16 @@ $(function () {
     function getCurrentHour() {
       return dayjs().format('H');
     }
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+// Add click event listener to the save buttons to save user input in local storage.
+    $('.saveBtn').on('click', function () {
+      // Get the hour from the parent time-block element's ID (e.g., 'hour-9' -> '9')
+      const hour = $(this).parent().attr('id').split('-')[1];
+      // Get the user input from the textarea within the same time-block
+      const userInput = $(this).siblings('.description').val();
+  
+      // Save the user input in local storage using the hour as the key
+      localStorage.setItem(`hour-${hour}`, userInput);
+    });
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
